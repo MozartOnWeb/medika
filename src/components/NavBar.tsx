d388a75bf1026252dd1123edc712a054e908c1af
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-09-03 23:35:11 +0000

    NavBar skeleton is done

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,37 @@
 export default function NavBar() {
   return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
+    <section className="navbar">
+      <div className="navbar__left">
+        <p>Medika</p>
+        <div>
+          <a href="#">Meditations</a>
+          <a href="#">Mission</a>
+          <a href="#">Purpose</a>
+          <a href="#">Articles</a>
+          <a href="#">About us</a>
+        </div>
+      </div>
+
+      <div className="navbar__right">
+        <button className="register">Register for free</button>
+        <button className="menu">
+          <svg
+            width="16"
+            height="8"
+            viewBox="0 0 16 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1 7H15M1 1H15"
+              stroke="white"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+      </div>
+    </section>
   );
 }
